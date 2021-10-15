8873b2803bd5caf6d92b46592ffebd625b6fe409
luca-iachini <luca.iachini@patchai.io>
2021-10-15 16:43:48 +0200

    fix sensor

--- go/app1/main.go
+++ go/app1/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "App1 greatings!!")
+	fmt.Fprintf(w, "App1 greatings!!!")
 }
 
 func health(w http.ResponseWriter, r *http.Request) {
